4fbe0af9b7641901bfa6ec571d5a685d2813912a
Unsighted-Shadow <carolsoeiro@gmail.com>
2024-11-03 13:51:07 -0300

    alpha 0.6.0

--- intranet/signin_up/intranet_login.js
+++ intranet/signin_up/intranet_login.js
@@ -1,19 +1,39 @@
-function user_login() {
+function user_login(event) {
+    event.preventDefault();
 
     var users = JSON.parse(localStorage.getItem("signups"));
-    console.log(users);
 
-    var cpf = document.getElementById("cpf").value
-    var password = document.getElementById("password").value
+    var category = document.getElementById("category").value;
+    var cpf = document.getElementById("cpf").value;
+    var password = document.getElementById("password").value;
 
-    var index = users.signup.findIndex(user => user.cpf === cpf);
+    var index = users.signup.findIndex(user => user.credential === cpf);
+
+    cleanFields();
+
     if (index !== -1) {
         var pass_crypto = CryptoJS.SHA1(password).toString(CryptoJS.enc.Base64);
         if (users.signup[index].password === pass_crypto) {
-            window.open("../intranet_homepages/cliente.html");
-            console.log(users.signup[index].password, pass_crypto)
+            switch (category) {
+                case "pax":
+                    window.open("../intranet_homepages/cliente.html", "_self");
+                    break;
+                case "employee":
+                case "provider":
+                    window.open("../intranet_homepages/pagina_inicial.html", "_self");
+                    break;
+                default:
+                    window.open("intranet_login.html", "_self")
+            }
+            
+            
         }
     }
 
 
+}
+
+function cleanFields() {
+    document.getElementById("cpf").value = "";
+    document.getElementById("password").value = "";
 }
